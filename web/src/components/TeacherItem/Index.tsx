e3c7c582fc73683945b1499c173991ed17b4ac56
edupferraz <edupferraz@gmail.com>
2020-08-05 18:29:08 -0300

    Conexão backend e frontend

--- web/src/components/TeacherItem/Index.tsx
+++ web/src/components/TeacherItem/Index.tsx
@@ -3,35 +3,59 @@ import React from 'react';
 import whatsIcon from '../../assets/images/icons/whatsapp.svg';
 
 import "./styles.css";
+import api from '../../services/api';
 
-function TeacherItem(){
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+          user_id: teacher.id,
+        });
+      }
+      
     return(
         <article className="teacher-item">
-        <header>
-            <img src="https://avatars1.githubusercontent.com/u/57379069?s=460&u=86db1b02e30d9f49bce7e09047cb8a2a3fdc0c87&v=4" alt="Eduardo Ferraz"/>
+            <header>
+                <img src={teacher.avatar} alt={teacher.name}/>
 
-            <div>
-                <strong>Eduardo Ferraz</strong>
-                <span> Quimica </span>
-            </div>
-        </header>
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span> {teacher.subject} </span>
+                </div>
+            </header>
 
-        <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas <br/> através de experiências e estudo.
-        </p>
-        
-        <footer>
             <p>
-                Preço/hora <strong>R$80,00</strong>
+                {teacher.bio}    
             </p>
-            <button type="button">
-                Entrar em contato
-                <img src={whatsIcon} alt="Whatsapp"/>
-            </button>
-        </footer>
-    </article>
+            
+            <footer>
+                <p>
+                    Preço/hora <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a  
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https:wa.me/${teacher.whatsapp}`}>
+                    Entrar em contato
+                    <img src={whatsIcon} alt="Whatsapp"/>
+                </a>
+            </footer>
+        </article>
     )
 }
 
