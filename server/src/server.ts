32e9710e6df700542251fd39f1f7103c0678fa50
edupferraz <edupferraz@gmail.com>
2020-08-04 22:37:45 -0300

    Finalização das funcionalidades e ajustes no Web

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-app.post('/', (request, response) => {    
-    return response.json({message: "Hello World"});
-});
-
-//localhost:3333
 app.listen(3333);
 
